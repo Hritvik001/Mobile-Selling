e0a9183f4a7aeea49d980f2350fda4b9137463b0
dhruva3223 <dhruva3223@gmail.com>
2022-07-05 14:42:28 +0530

    added register, login, logout.

--- Login_and_Register/settings.py
+++ Login_and_Register/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9z6ewt#j86_qkhn^cmd(j@i$nv7d2!vj3!^ud=lv3mk=nrmknb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.106']
 
 
 # Application definition
